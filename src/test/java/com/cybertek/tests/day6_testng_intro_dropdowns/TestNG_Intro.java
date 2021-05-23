97d180ae8b5da56aab748c13d764a1042daad975
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-05-23 16:48:44 -0400

    TestNG_Intro.java first day complete

--- src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
@@ -1,15 +1,38 @@
 package com.cybertek.tests.day6_testng_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-        @Test
-        public void test1(){
-            System.out.println("Running test 1");
+
+    @Test(priority = 1)
+    public void test1() {
+        System.out.println("Running test 1...");
+
+        String actual = "apple";
+        String expected = "apples";
+
+        Assert.assertEquals(actual, expected);
+
+
     }
-    @Test
-    public void test2(){
-        System.out.println("Running test 2 ");
+
+    @Test(priority = 2)
+    public void test2() {
+        System.out.println("Running test 2...");
+        String actual = "apple";
+        String expected = "apples";
+
+        Assert.assertTrue(actual.equals(expected));
+        //Assert.assertFalse(false);
+
+    }
+
+    @BeforeClass
+    public void setupClass() {
+        System.out.println("-----> Before CLASS is running...");
+        System.out.println("---> After method is running...");
     }
 }
+
