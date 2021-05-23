83cff78a0edb57645dba3c47200eabc11d1b96c5
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-05-23 14:56:55 -0400

    TestNG_Intro.java first day

---
+++ src/test/java/com/cybertek/tests/day6_testng_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day6_testng_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+        @Test
+        public void test1(){
+            System.out.println("Running test 1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Running test 2 ");
+    }
+}
