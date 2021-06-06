d36c694098f71c413d5f6d725d0731c7d845833b
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-06-06 16:54:35 -0400

    ConfigurationReader added

---
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -0,0 +1,17 @@
+package com.cybertek.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+   private static Properties properties = new Properties();
+    static {
+        try {
+            FileInputStream file=new FileInputStream("configuration.properties");
+            properties.load(file);
+        } catch (IOException e) {
+            System.out.println("File not found in configuration properties");
+        }
+    }
+}
