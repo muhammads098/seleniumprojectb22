4e27908e62e689eb1da8e1c20c98728d12156957
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-06-16 20:54:37 -0400

    added work

--- src/test/java/com/cybertek/utilities/TableUtils.java
+++ src/test/java/com/cybertek/utilities/TableUtils.java
@@ -35,7 +35,28 @@ public class TableUtils {
             }
         }
 
+        //Assert.fail("this line will fail the code");
+
         Assert.assertTrue(false, "Name does not exist in the list.");
+    }
+
+    //task 5 - create new method to print out names and emails
+    public static void printNamesAndEmails(WebDriver driver){
+
+        //Create a locator that returns all the names in the table
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='table1']//tr/td[2]"));
+
+        //Create a locator that returns all the emails in the list
+        List<WebElement> allEmails = driver.findElements(By.xpath("//table[@id='table1']//tr/td[3]"));
+
+        for (int i = 0; i < allNames.size(); i++) {
+
+            System.out.println("Name \t: " + allNames.get(i).getText());
+            System.out.println("Email \t: " + allEmails.get(i).getText());
+            //System.out.println("Name " + i + ": " + allNames.get(i) + ". Email " + i + ": " + allEmails.get(i));
+        }
+
+
     }
 
 
