ad84683b1b2e50434908fe0c3082d1bbd3c2cac5
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-06-26 14:55:02 -0400

    Actions Practice

--- src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day14_pom_synchronization;
 
+import com.cybertek.pages.DynamicLoad1Page;
 import com.cybertek.pages.DynamicLoad7Page;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -30,5 +31,30 @@ public class WebDriverWaitPractice {
 //        4. Assert : Image is displayed.
         Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
 
+    }
+
+    @Test
+    public void dynamic_loading_1_test() {
+        //1. Go to http://practice.cybertekschool.com/dynamic_loading/1
+        Driver.getDriver().get("http://practice.cybertekschool.com/dynamic_loading/1");
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //2. Click to start
+        dynamicLoad1Page.startButton.click();
+
+        //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+        //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.InputUsername.isDisplayed());
+
+        //5. Enter username: tomsmith
+        dynamicLoad1Page.InputUsername.sendKeys("tomsmith");
+        //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("incorrectPassword");
+        //7. Click to Submit button
+        dynamicLoad1Page.submitButton.click();
+        //8. Assert “Your password is invalid!” text is displayed.
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
     }
 }
